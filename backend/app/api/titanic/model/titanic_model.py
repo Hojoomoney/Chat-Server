aba52eceec50337f76fe71d20a0a19ede3aecf02
Hojoomoney <kimhojoo51@gmail.com>
2024-05-14 18:45:31 +0900

    Feat : add Logic (titanic_model)

--- backend/app/api/titanic/model/titanic_model.py
+++ backend/app/api/titanic/model/titanic_model.py
@@ -1,49 +1,96 @@
-from dataclasses import dataclass
+import pandas as pd
+from app.api.context.data_sets import DataSets
+from app.api.context.models import Models
+import numpy as np
 
-@dataclass
-class TitanicModel:
-    _context: str = ''
-    _fname: str = ''
-    _train: object = None
-    _test: object = None
-    _id: str = ''
-    _label: str = ''
 
-    @property
-    def context(self) -> str: return self._context 
+class TitanicModel(object):
+    model = Models()
+    dataset = DataSets()
 
-    @context.setter
-    def context(self, context): self._context = context
-
-    @property
-    def fname(self) -> str: return self._fname
-
-    @fname.setter
-    def fname(self, fname): self._fname = fname
-
-    @property
-    def train(self) -> object: return self._train
-
-    @train.setter
-    def train(self, train): self._train = train
-
-    @property
-    def test(self) -> object: return self._test
-
-    @test.setter
-    def test(self, test): self._test = test
-
-    @property
-    def id(self) -> str: return self._id
-
-    @id.setter
-    def id(self, id): self._id = id
-
-    @property
-    def label(self) -> str: return self._label
-
-    @label.setter
-    def label(self, label): self._label = label
-
-         
+    def preprocess(self, train_fname, test_fname) -> pd.DataFrame:
+        this = self.dataset
+        that = self.model
+        this.dataset.train = self.new_model(train_fname)
+        this.dataset.test = self.new_model(test_fname)
+        feature = ['PassengerId', 'Survived', 'Pclass', 'Name','Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+        this.train = that.new_dframe(train_fname)
+        this.test = that.new_dframe(test_fname)
+        this.id = this.test['PassengerId']
+        this.label = this.train['Survived']
+        this.train = this.train.drop('Survived', axis=1)
+        this = self.drop_feature(this, 'Name', 'SibSp', 'Parch', 'Cabin', 'Ticket')
+        
+        return this.dataset
     
+    @staticmethod
+    def drop_feature(this, *feature) -> pd.DataFrame: # feature는 가변인자 자바에서는 필드라고 함 db에서는 컬럼이라고 함
+        
+        # for i in feature:
+        #     this.train = this.train.drop(i, axis=1) # feature에 있는 컬럼을 삭제 [i]이렇게 하면 인덱스로 인식
+        #     this.test = this.test.drop(i, axis=1)
+        
+        # for i in [this.train, this.test]:
+        #     for j in feature:
+        #         i.drop(j, axis=1, inplace=True) # inplace=True 원본을 삭제하겠다는 의미
+        
+        [i.drop(j, axis=1, inplace=True) for j in feature for i in [this.train, this.test]]
+
+        return this
+    
+    @staticmethod
+    def extract_title_from_name(this) -> pd.DataFrame:
+        for i in [this.train, this.test]:
+            i['Title'] = i['Name'].str.extract('([A-Za-z]+)\.', expand=False) #Mr. Mrs. Miss. Master. Dr. Capt. 등을 추출
+        return this
+    
+    @staticmethod
+    def remove_duplicate_title(this) -> pd.DataFrame:
+        a = []
+        for these in [this.train, this.test]:
+            a += list(set(these['Title']))
+        a = list(set(a))
+        print(a)
+        '''
+        ['Mr', 'Sir', 'Major', 'Don', 'Rev', 'Countess', 'Lady', 'Jonkheer', 'Dr',
+        'Miss', 'Col', 'Ms', 'Dona', 'Mlle', 'Mme', 'Mrs', 'Master', 'Capt']
+        Royal : ['Countess', 'Lady', 'Sir']
+        Rare : ['Capt','Col','Don','Dr','Major','Rev','Jonkheer','Dona','Mme' ]
+        Mr : ['Mlle']
+        Ms : ['Miss']
+        Master
+        Mrs
+        '''
+        title_mapping = {'Mr':1, 'Ms':2, 'Mrs':3, 'Master':4, 'Royal':5, 'Rare':6}
+        return title_mapping
+    
+    @staticmethod
+    def title_nominal(this,title_mapping) -> pd.DataFrame:
+        for these in [this.train, this.test]:
+            these['Title'] = these['Title'].replace(['Countess', 'Lady', 'Sir'], 'Royal')
+            these['Title'] = these['Title'].replace(['Capt','Col','Don','Dr','Major','Rev','Jonkheer','Dona','Mme'], 'Rare')
+            these['Title'] = these['Title'].replace(['Mlle'], 'Mr')
+            these['Title'] = these['Title'].replace(['Miss'], 'Ms')
+            # Master 는 변화없음
+            # Mrs 는 변화없음
+            these['Title'] = these['Title'].fillna(0)
+            these['Title'] = these['Title'].map(title_mapping)
+        return this
+    
+    @staticmethod
+    def age_ratio(this) -> pd.DataFrame:
+        train = this.train
+        test = this.test
+        age_mapping = {'Unknown':0 , 'Baby': 1, 'Child': 2, 'Teenager' : 3, 'Student': 4,
+                       'Young Adult': 5, 'Adult':6,  'Senior': 7}
+        train['Age'] = train['Age'].fillna(-0.5)
+        test['Age'] = test['Age'].fillna(-0.5) # 왜 NaN 값에 -0.5 를 할당할까요 ?
+        bins = [-1, 0, 5, 12, 18, 24, 35, 60, np.inf] # 이것을 이해해보세요 bins란 구간를 나타냄 
+        labels = ['Unknown', 'Baby', 'Child', 'Teenager', 'Student', 'Young Adult', 'Adult', 'Senior']
+
+        for these in train, test:
+            pass # pd.cut() 함수를 사용해보세요 다른 곳은 고치지 말고 다음 두 줄만 완성하세요
+            these['Age'] = pd.cut(these['Age'], bins, labels=labels) # Age를 구간화 시킴 
+            these['AgeGroup'] = these['Age'].map(age_mapping) # AgeGroup을 만들어서 Age를 매핑시킴 Nominal로 만들어줌
+
+        return this
--- backend/app/api/titanic/service/titanic_service.py
+++ backend/app/api/titanic/service/titanic_service.py
@@ -9,13 +9,22 @@ class TitanicService:
     def process(self): #self를 하면 인스턴스 변수를 사용할 수 있음 즉 등록된 변수를 사용할 수 있음 
         print('프로세스 시작')
         this = self.model # this는 property 자바에서는 getter setter ,self는 자바에서는 this
-        this.train = self.new_model('train.csv') # this.train_model은 자바에서는 this.trainModel
+        feature = ['PassengerId', 'Survived', 'Pclass', 'Name','Sex', 'Age', 'SibSp', 'Parch', 'Ticket', 'Fare', 'Cabin', 'Embarked']
+        this.train = self.new_model('train.csv') 
         this.test = self.new_model('test.csv')
         self.df_info(this) # 데이터 프레임의 정보를 출력
         
         this.id = this.test['PassengerId'] # 문제를 제출할 때 필요한 id
 
+        this = self.name_nominal(this)
+
         this = self.drop_feature(this, 'Name', 'SibSp', 'Parch', 'Cabin', 'Ticket')
+
+        this = self.pclass_ordinal(this)
+        this = self.embarked_nominal(this)
+        this = self.age_ratio(this)
+        this = self.sex_nominal(this)
+        this = self.fare_ratio(this)
         
         self.df_info(this)
 
@@ -25,11 +34,7 @@ class TitanicService:
     def df_info(this): #self가 없으면 static 메소드
         [print(i.info()) for i in [this.train, this.test]]
 
-    def new_model(self, payload) -> object:
-        this = self.model
-        this.context = './app/api/titanic/data/'
-        this.fname = payload
-        return pd.read_csv(this._context + this._fname)
+
 
     @staticmethod
     def create_train(this) -> str:  
@@ -39,18 +44,30 @@ class TitanicService:
     def create_label(this) -> str:
         return this.train['Survived'] # 결과데이터셋
     
-    @staticmethod
-    def drop_feature(this, *feature) -> object: # feature는 가변인자 자바에서는 필드라고 함 db에서는 컬럼이라고 함
-        
-        # for i in feature:
-        #     this.train = this.train.drop(i, axis=1) # feature에 있는 컬럼을 삭제 [i]이렇게 하면 인덱스로 인식
-        #     this.test = this.test.drop(i, axis=1)
-        
-        # for i in [this.train, this.test]:
-        #     for j in feature:
-        #         i.drop(j, axis=1, inplace=True) # inplace=True 원본을 삭제하겠다는 의미
-        
-        [i.drop(j, axis=1, inplace=True) for j in feature for i in [this.train, this.test]]
 
+    
+    @staticmethod
+    def pclass_ordinal(this) -> object:
         return this
-        
+    @staticmethod
+    def extract_title_from_name(this) -> object:
+        combine = [this.train, this.test]
+        for i in combine:
+            i['Title'] = i['Name'].str.extract('([A-Za-z]+)\.', expand=False)
+        return this
+
+    @staticmethod
+    def name_nominal(this) -> object:
+        return this
+    @staticmethod
+    def sex_nominal(this) -> object:
+        return this
+    @staticmethod
+    def age_ratio(this) -> object:
+        return this
+    @staticmethod
+    def embarked_nominal(this) -> object:
+        return this
+    @staticmethod
+    def fare_ratio(this) -> object:
+        return this
